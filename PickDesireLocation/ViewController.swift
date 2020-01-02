85b6b60e8b3a2c1bdd4da85a0d295af74f6b32b6
Ali Raza <>
2020-01-02 12:43:12 +0500

    Get current location + Pick your desire location by tap on Map view.

---
+++ PickDesireLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickDesireLocation
+//
+//  Created by Ali Raza Amjad on 02/01/2020.
+//  Copyright © 2020 Ali Raza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
